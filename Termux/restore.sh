44d9f57aea0022903109814753c3eef15077f966
P <30462819+ptrpfa@users.noreply.github.com>
2022-03-20 16:46:06 +0800

    added termux scripts

--- Termux/restore.sh
+++ Termux/restore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Simple script to restore Termux from a backup file
-tar -zxf $1 -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
+tar -zvxf $1 -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
