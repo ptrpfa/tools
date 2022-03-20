8a9e19c2d7af5998df645c0a420801862e636e67
P <30462819+ptrpfa@users.noreply.github.com>
2022-03-20 17:09:54 +0800

    update termux files

--- Termux/backup.sh
+++ Termux/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Simple script to create a Termux backup
-# Usage: ./backup.sh <file name>
-tar -zvcf /storage/downloads/$1.tar.gz -C /data/data/com.termux/files ./home ./usr
+# Usage: ./backup.sh <file path>.tar.gz
+tar -zcvf $1.tar.gz -C /data/data/com.termux/files ./home ./usr
--- Termux/restore.sh
+++ Termux/restore.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Simple script to restore Termux from a backup file
-tar -zvxf $1 -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
+# Usage ./restore.sh <file path>
+tar -zxvf $1 -C /data/data/com.termux/files --recursive-unlink --preserve-permissions
